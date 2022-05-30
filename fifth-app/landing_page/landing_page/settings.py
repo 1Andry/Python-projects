770d6bafc571423870f7853674efa5fc1effe41b
Andry <andry@MacBook-Air-Andry.local>
2022-05-30 21:13:11 +0300

    add 5 project

--- fifth-app/landing_page/landing_page/settings.py
+++ fifth-app/landing_page/landing_page/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'cms.apps.CmsConfig'
+    'cms.apps.CmsConfig',
+    'price.apps.PriceConfig',
+    'crm.apps.CrmConfig',
 ]
 
 MIDDLEWARE = [
--- thirg-app/todos/todo/views.py
+++ thirg-app/todos/todo/views.py
@@ -47,7 +47,7 @@ def loginuser(request):
     if request.method == 'GET':
         return render(request, 'todo/loginuser.html', {'form': AuthenticationForm()})
     else:
-        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        user = authenticate(request, email=request.POST['email'], password=request.POST['password'])
         if user is None:
             return redirect(request, 'todo/loginuser.html', {'form': AuthenticationForm(),
                                                              'error': 'Неверные данные для входа'})
